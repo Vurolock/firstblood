d67063b33e9427a95532d1e4a999512be2d82404
Andrew Keller <Keller192@gmail.com>
2018-02-15 15:43:23 -0500

    add axios GET request to index using dotenv to protect api key

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+require('dotenv').config()
+const axios = require('axios');
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', (req, res) => {
-    
-});
+const API_KEY = process.env.API_KEY;
+
+// router.all('*', (req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//     next();
+// })
+
+router.route('/')
+    .get((req, res) => {
+        axios.get(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/vurolok?api_key=${API_KEY}`)
+            .then((data) => {
+                res.json(data.data);
+            })
+    });
 
 module.exports = router;
