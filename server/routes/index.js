c521ad217bd408d011b9e8cb6d5f152fddfc5fcb
Andrew Keller <Keller192@gmail.com>
2018-02-27 17:19:16 -0500

    adds request-promise and cheerio to parse web page

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const axios = require('axios');
 const express = require('express');
+const rp = require('request-promise');
+const cheerio = require('cheerio');
 const router = express.Router();
 
 const API_KEY = process.env.API_KEY;
@@ -14,13 +16,51 @@ const API_KEY = process.env.API_KEY;
 
 router.route('/')
     .get((req, res) => {
-        axios.get(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/vurolok?api_key=${API_KEY}`)
+        axios.get(`https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/shiphtur?api_key=${API_KEY}`)
             .then((summonerData) => {
-                axios.get(`https://na1.api.riotgames.com/lol/match/v3/matchlists/by-account/${summonerData.data.accountId}?api_key=${API_KEY}`)
-                    .then((matchlistData) => {
-                        res.json(matchlistData.data);
+                axios.get(`https://na1.api.riotgames.com/lol/spectator/v3/active-games/by-summoner/${summonerData.data.id}?api_key=${API_KEY}`)
+                    .then((spectator) => {
+                        res.json(spectator.data.participants);
                     });
             });
     });
 
+
+router.route('/scrape')
+    .get((req, res) => {
+        const options = {
+            uri: 'http://na.op.gg/summoner/champions/userName=Vurolok',
+            transform: (body) => cheerio.load(body)
+        };
+        rp(options)
+            .then(($) => {
+                data = $('.Body').text();
+                return data;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .then((data) => {
+                res.json(data.replace(/[\r\n\t]/g, ""));
+            })
+
+    });
+
+router.route('/favicon.ico')
+    .get((req, res) => {
+        res.json('blah');
+    })
+
 module.exports = router;
+
+
+/*
+    Endpoint Chain:
+
+    Submit input               -->  /lol/summoner/v3/summoners/by-name/{summonerName}       (1)  returns:
+    summonerId + accountId[0]  -->  /lol/spectator/v3/active-games/by-summoner/{summonerId} (1)  returns:
+    summonerName[1-9]          -->  /lol/summoner/v3/summoners/by-name/{summonerName}       (9)  returns:
+    accountId[1-9]             -->  /lol/match/v3/matchlists/by-account/{accountId}        (10)  returns:
+    gameId[0-n] + champion     -->  /lol/match/v3/matches/{matchId}                         (n)  returns:
+    K/D/A + CS + W/L           -->  Database  -->  Client
+*/
